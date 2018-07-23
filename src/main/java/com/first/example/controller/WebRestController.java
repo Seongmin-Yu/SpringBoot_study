df3024dc906dc2103a148c3cae6ce88f8903cde5
Seongmin-Yu <tjdalsdbqqq@naver.com>
2018-07-23 11:36:14 +0900

    Insert_Data_test_with_JPA

--- src/main/java/com/first/example/controller/WebRestController.java
+++ src/main/java/com/first/example/controller/WebRestController.java
@@ -1,13 +1,22 @@
 package com.first.example.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import com.first.example.domain.posts.PostsRepository;
+import com.first.example.dto.PostsSaveRequestDto;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@AllArgsConstructor
 public class WebRestController {
+    private PostsRepository postsRepository;
+
     @GetMapping("/hello/{name}")
-    public String Hello(@PathVariable String name){
-        return "Hello, "+name+"Welcome to Spring Boot";
+    public String Hello(@PathVariable String name) {
+        return "Hello, " + name + "Welcome to Spring Boot";
+    }
+
+    @PostMapping("/posts")
+    public void savePosts(@RequestBody PostsSaveRequestDto dto) {
+        postsRepository.save(dto.toEntity());
     }
 }
